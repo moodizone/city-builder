a9adc8140e4e044bd04e311ee0430c0539769b82
moodi <moodizone@gmail.com>
2025-02-24 15:56:59 +0330

    🚧 attach handlers to menu actions

--- src/components/sidebar/menu.tsx
+++ src/components/sidebar/menu.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
+import { ModeType } from "./item";
 
-function Menu() {
+interface Props {
+  modeHandler(mode: ModeType): void;
+  duplicateHandler(): void;
+}
+
+function Menu({ modeHandler, duplicateHandler }: Props) {
   return (
     <div
       className="absolute right-0 w-32 bg-zinc-700 rounded shadow-lg z-10 overflow-hidden"
@@ -9,18 +15,21 @@ function Menu() {
       <button
         className="block w-full px-4 py-2 text-sm text-white hover:bg-gray-600 text-left"
         role="menuitem"
+        onClick={() => modeHandler("rename")}
       >
         {"Rename"}
       </button>
       <button
         className="block w-full px-4 py-2 text-sm text-white hover:bg-gray-600 text-left"
         role="menuitem"
+        onClick={duplicateHandler}
       >
-        {"Archive"}
+        {"Duplicate"}
       </button>
       <button
         className="block w-full px-4 py-2 text-sm text-red-600 hover:bg-red-100 text-left"
         role="menuitem"
+        onClick={() => modeHandler("remove")}
       >
         {"Delete"}
       </button>
