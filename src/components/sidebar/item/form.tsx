dbd0569b5902d4ca1da5f494ffee559e5cf9144a
moodi <moodizone@gmail.com>
2025-02-24 15:55:49 +0330

    🚧 add form for renaming the house

---
+++ src/components/sidebar/item/form.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+
+import Input from "@/components/input";
+
+interface Props {
+  initialValue: string;
+  renameHandler(name: string): void;
+}
+
+function Form({ initialValue = "", renameHandler }: Props) {
+  const ref = React.useRef<HTMLInputElement>(null);
+  const [value, onChange] = React.useState<string>(initialValue);
+
+  // auto-focus and auto-select on input when component mounts
+  React.useEffect(() => {
+    if (ref.current) {
+      ref.current.focus();
+      ref.current.select();
+    }
+  }, []);
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        renameHandler(value);
+      }}
+    >
+      <Input
+        ref={ref}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </form>
+  );
+}
+
+export default Form;
