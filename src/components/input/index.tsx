309f67bde03d052c77afe7d5d75c983102c761e4
moodi <moodizone@gmail.com>
2025-02-24 15:54:35 +0330

    ⚰️ remove error message prop (simplification

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,32 +1,22 @@
-import classNames from "classnames";
 import * as React from "react";
+import classNames from "classnames";
 
-interface Props
-  extends React.DetailedHTMLProps<
-    React.InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
-  > {
-  // hint error message
-  message?: string;
-}
-
-function Input({ className: cls, message, ...others }: Props) {
+function Input({
+  className: cls,
+  ...others
+}: React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>) {
   return (
-    <>
-      <input
-        {...others}
-        className={classNames(
-          cls,
-          `border text-sm rounded-lg focus:ring-red-500
-           dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 text-red-500 placeholder-red-500 border-red-500`
-        )}
-      />
-      {message ? (
-        <p className="mt-2 text-sm text-red-600 dark:text-red-500">
-          <span className="font-medium">{message}</span>
-        </p>
-      ) : null}
-    </>
+    <input
+      {...others}
+      className={classNames(
+        cls,
+        `border text-sm rounded lock w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400
+         text-white focus:ring-blue-500 focus:border-blue-500`
+      )}
+    />
   );
 }
 
