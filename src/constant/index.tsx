6bb7475d3cb668ca222672416a1ba2af186c4936
moodi <moodizone@gmail.com>
2025-02-24 18:38:37 +0330

    🚧 add app constant

---
+++ src/constant/index.tsx
@@ -0,0 +1,8 @@
+import { colors } from "@/utils/random";
+
+export const MAX_FLOOR = 6;
+export const MAX_ROOM = 4;
+
+export const FALLBACK_FLOOR = 1;
+export const FALLBACK_ROOM = 2;
+export const FALLBACK_COLOR = colors[0];
